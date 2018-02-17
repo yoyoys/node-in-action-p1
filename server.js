4157e4f539260a970a808ea504889c8df8d57947
yoyoys <a9565566@gmail.com>
2018-02-17 12:30:22 +0800

    chat setvet完成

--- server.js
+++ server.js
@@ -4,21 +4,20 @@ const http = require("http"),
   mime = require("mime"),
   cache = {};
 
-const server = http.createServer((req, res)=>{
+const server = http.createServer((req, res) => {
   let filePath = false;
-  if(req.url === '/')
-    filePath = 'public/index.html';
-  else
-    filePath = 'public' + req.url;
+  if (req.url === "/") 
+    filePath = "public/index.html";
+  else  
+    filePath = `public${req.url}`;
 
-  let absPath = './' + filePath;
+  let absPath = `./${filePath}`;
   serverStatic(res, cache, absPath);
 });
 
-server.listen(3000,()=>{
-  console.log('Server listening on port 3000');
+server.listen(3000, () => {
+  console.log("Server listening on port 3000");
 });
-
 
 function send404(res) {
   res.writeHead(404, { "Contrnt-Type": "text/plain" });
@@ -31,22 +30,19 @@ function sendFile(res, filePath, fileContents) {
   res.end(fileContents);
 }
 
-function serverStatic(res, cache, absPath) {  
-  if(cache[absPath])
-    sendFile(res, absPath, cache[absPath])
-  else{
-    fs.exists(absPath, (exists)=>{
-      if(exists){
-        fs.readFile(absPath, (err, data)=>{
-          if(err)
-            send404(res);
-          else{
+function serverStatic(res, cache, absPath) {
+  if (cache[absPath]) sendFile(res, absPath, cache[absPath]);
+  else {
+    fs.exists(absPath, exists => {
+      if (exists) {
+        fs.readFile(absPath, (err, data) => {
+          if (err) send404(res);
+          else {
             cache[absPath] = data;
             sendFile(res, absPath, data);
           }
         });
-      }else
-        send404(res);
-    })
+      } else send404(res);
+    });
   }
 }
